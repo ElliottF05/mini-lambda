9da4c29f34e73a76caafc14eef685be0e87ae1ac
ElliottF05 <elliotthfaa@gmail.com>
2026-03-12 21:35:51 -0400

    feat: use clap for client cli, and improve error handling

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,28 +1,41 @@
 use tonic::Request;
+use clap::Parser;
 
 use shared::client_api_client::ClientApiClient;
 use shared::executor_client::ExecutorClient;
 use shared::{JobRequest, WorkerRequest};
 
-#[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cli_args : Vec<_>= std::env::args().skip(1).collect();
-    let wasm_path = &cli_args[0];
-    let wasm_args = &cli_args[1..];
+/// Cli args for using clap
+#[derive(Parser, Debug)]
+#[command(about = "Submit a wasm job to the distributed compute platform")]
+struct Args {
+    wasm_path: String,
+    #[arg(long, default_value = "http://127.0.0.1:50051")]
+    orchestrator: String,
+    wasm_args: Vec<String>
+}
 
-    let orchestrator_endpoint: &str = "http://127.0.0.1:50051";
-    let mut orchestrator_client = ClientApiClient::connect(orchestrator_endpoint).await?;
+/// The main cli entrypoint to the Client, allowing submission of a wasm job.
+#[tokio::main]
+pub async fn main() {
+    let args = Args::parse();
+
+    let orchestrator_endpoint = args.orchestrator;
+    let mut orchestrator_client = ClientApiClient::connect(orchestrator_endpoint).await
+        .unwrap_or_else(|e| panic!("Failed to connect to the Orchestrator: {}", e));
 
     let worker_request = WorkerRequest {};
 
-    let response = orchestrator_client.request_worker(Request::new(worker_request)).await?;
+    let response = orchestrator_client.request_worker(Request::new(worker_request)).await
+        .unwrap_or_else(|e| panic!("Failed to request a worker from the Orchestrator: {}", e));
     let worker_address = response.into_inner().worker_address; 
     let worker_endpoint = "http://".to_string() + &worker_address;
     println!("Worker endpoint: {}", worker_endpoint);
 
-    let wasm_bytes = std::fs::read(wasm_path).unwrap();
-    let mut executor_client = ExecutorClient::connect(worker_endpoint).await?;
-    let job_request = JobRequest { wasm_bytes: wasm_bytes, args: wasm_args.to_vec() };
+    let wasm_bytes = std::fs::read(args.wasm_path).unwrap();
+    let mut executor_client = ExecutorClient::connect(worker_endpoint).await
+        .unwrap_or_else(|e| panic!("Failed to connect to the provided Worker: {}", e));
+    let job_request = JobRequest { wasm_bytes: wasm_bytes, args: args.wasm_args };
 
     let execution_result = executor_client.execute_job(Request::new(job_request)).await;
     match execution_result {
@@ -32,10 +45,10 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Err(e) => {
             match e.code() {
                 tonic::Code::InvalidArgument => {
-                    println!("Job produced an error at compilation or runtime: {}", e.message());
+                    eprintln!("Job produced an error at compilation or runtime: {}", e.message());
                 },
                 tonic::Code::Internal => {
-                    println!("Worker failed with error: {}", e.message());
+                    eprintln!("Worker failed with error: {}", e.message());
                 }
                 _ => {
                     eprintln!("Received an unexepected error code {}, with message: {}", e.code(), e.message());
@@ -43,6 +56,4 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     }
-
-    Ok(())
 }
